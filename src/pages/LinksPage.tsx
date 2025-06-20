d040808215bff1c3d25273ea84b349ae6aaa9224
Shea Riley <sheariley@live.com>
2025-06-20 16:24:38 -0500

    Add farm-hub website to links; revamp links page format

--- src/pages/LinksPage.tsx
+++ src/pages/LinksPage.tsx
@@ -1,6 +1,11 @@
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { faCode, faFileText } from '@fortawesome/free-solid-svg-icons';
+import {
+  faCode,
+  faExternalLink,
+  faFileText
+} from '@fortawesome/free-solid-svg-icons';
 
+import farmHubWebsitePreviewImg from '@/assets/website-screenshots/farm-hub-home-page-screenshot.jpg';
 import Button from '@/components/ui/Button';
 import Card from '@/components/ui/Card';
 import PageHeader from '@/components/ui/PageHeader';
@@ -9,16 +14,68 @@ import ExternalLinkUrls from '@/utils/external-link-urls';
 export default function LinksPage() {
   return (
     <>
-      <PageHeader>Links</PageHeader>
-      <br />
+      <PageHeader />
       <section className="flex flex-col items-stretch gap-8 space-y-12">
         <Card>
           <Card.Body>
-            <Card.Title className="mb-0 flex justify-center">
+            <Card.Title className="mb-0 flex justify-start">
+              Publicly Accessible Websites
+            </Card.Title>
+            <div className="flex flex-col items-start">
+              <ul className="my-0 list-none space-y-6 pl-0 *:flex *:min-w-[180px] *:flex-col *:items-start *:pl-0">
+                <li>
+                  <figure className="my-0 flex flex-col items-start">
+                    <a
+                      className="mb-2 self-start"
+                      target="_blank"
+                      rel="noreferrer"
+                      href={ExternalLinkUrls.FarmHub}
+                    >
+                      <img
+                        className="my-0 rounded-lg"
+                        width="240px"
+                        height="180px"
+                        src={farmHubWebsitePreviewImg}
+                        alt="FarmHub website preview"
+                      />
+                    </a>
+                    <Button
+                      themeColor="primary"
+                      href={ExternalLinkUrls.FarmHub}
+                      target="_blank"
+                      icon={faExternalLink}
+                      size="sm"
+                      className="justify-start self-start"
+                    >
+                      FarmHub - Farmers Market Directory
+                    </Button>
+                    <figcaption className="self-start text-base text-base-content">
+                      A directory website that lists farmers markets,
+                      farm-stands, and farms by state and city. The website was
+                      built using Next.js, NodeJS, Tailwind, daisyUI, Supabase,
+                      and Leaflet. Data was acquired via Google Maps Places API.
+                      The data was then filtered using automated AI workflows
+                      built in n8n and invoked from NodeJS scripts. The data
+                      filtering workflows integrated various open source LLMs
+                      via a locally running instance of Ollama. Logging was an
+                      important aspect of the data filtering process, as the AI
+                      system prompts had to be tuned to reduce false
+                      positives/negatives.
+                    </figcaption>
+                  </figure>
+                </li>
+              </ul>
+            </div>
+          </Card.Body>
+        </Card>
+
+        <Card>
+          <Card.Body>
+            <Card.Title className="mb-0 flex justify-start">
               Links &amp; Downloads
             </Card.Title>
-            <div className="mb-4 flex flex-col items-center">
-              <ul className="my-0 list-none pl-0 *:flex *:w-[180px] *:pl-0 md:flex md:flex-row md:space-x-4 md:*:my-4 max_md:space-y-6">
+            <div className="mb-4 flex flex-col items-start">
+              <ul className="my-0 list-none space-y-6 pl-0 *:flex *:min-w-[180px] *:pl-0">
                 <li>
                   <Button
                     themeColor="primary"
@@ -26,7 +83,7 @@ export default function LinksPage() {
                     download
                     icon={faFileText}
                     size="sm"
-                    className="w-full justify-start"
+                    className="justify-start"
                   >
                     Resume
                   </Button>
@@ -38,7 +95,7 @@ export default function LinksPage() {
                     target="_blank"
                     icon={faLinkedin}
                     size="sm"
-                    className="w-full justify-start"
+                    className="justify-start"
                   >
                     Linked-In profile
                   </Button>
@@ -50,7 +107,7 @@ export default function LinksPage() {
                     target="_blank"
                     icon={faGithub}
                     size="sm"
-                    className="w-full justify-start"
+                    className="justify-start"
                   >
                     GitHub profile
                   </Button>
@@ -62,7 +119,7 @@ export default function LinksPage() {
                     target="_blank"
                     icon={faCode}
                     size="sm"
-                    className="w-full justify-start"
+                    className="justify-start"
                   >
                     Site Source Code
                   </Button>
--- src/utils/external-link-urls.ts
+++ src/utils/external-link-urls.ts
@@ -1,7 +1,8 @@
 enum ExternalLinkUrls {
   LinkedIn = 'https://www.linkedin.com/in/henry-riley-8bb19336/',
   GitHub = 'https://github.com/sheariley',
-  Source = 'https://github.com/sheariley/portfolio-web-dev'
+  Source = 'https://github.com/sheariley/portfolio-web-dev',
+  FarmHub = 'https://farm-hub.net'
 }
 
 export default ExternalLinkUrls;
